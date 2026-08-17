0cfbe8f4f6b4fb6cc23455996b9f0550342daedd
jm9742 <123829174+jm9742@users.noreply.github.com>
2026-08-16 22:04:49 -0400

    Add: merge-k-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-0.py
@@ -0,0 +1,25 @@
+import heapq
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+
+        heap = []
+
+        # 先把每条 linked list 的 head 放进 heap
+        for i, node in enumerate(lists):
+            if node:
+                heapq.heappush(heap, (node.val, i, node))
+
+        dummy = ListNode()
+        current = dummy
+
+        while heap:
+            val, i, node = heapq.heappop(heap)
+
+            current.next = node
+            current = current.next
+
+            if node.next:
+                heapq.heappush(heap, (node.next.val, i, node.next))
+
+        return dummy.next
